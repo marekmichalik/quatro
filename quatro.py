ec3d7bf45692805079f3628c5aea63b8549a36aa
Marek Michalík <marek.michalik@nike.sk>
2021-02-14 23:30:24 +0100

    Final version, with webm video as result.

--- quatro.py
+++ quatro.py
@@ -1,5 +1,14 @@
+import os
 from random import randrange, choice
 from PIL import Image, ImageDraw
+
+FRAMES = 1000  # number of frames of viedo
+SQUARES = 100  # squares count
+SIZE_X = 1920  # video dimensions in pixels
+SIZE_Y = 1080
+TEMP_FILE = 'quatro.gif'
+OUTPUT_FILE = 'quatro.webm'
+
 
 class square:
     def __init__(self, ax, ay, bx, by, dx, dy):
@@ -14,15 +23,14 @@ class square:
     def __str__(self):
         return f'a[{self.ax}, {self.ay}], b[{self.bx}, {self.by}]: [{self.dx}, {self.dy}]'
 
-    def intersects(self, ax, ay, bx, by, sizex, sizey):
-
+    def intersects(self, ax, ay, bx, by):
         if ax < 0:
             return True
         if ay < 0:
             return True
-        if bx >= sizex:
+        if bx >= SIZE_X:
             return True
-        if by >= sizey:
+        if by >= SIZE_Y:
             return True
 
         # they are "side by side" :)
@@ -44,11 +52,10 @@ class square:
 
         return True
 
-    def intersects_vertically(self, ax, ay, bx, by, sizex, sizey):  # dx should change
-
+    def intersects_vertically(self, ax, ay, bx, by):  # dx should change
         if ax < 0:
             return True
-        if bx >= sizex:
+        if bx >= SIZE_X:
             return True
 
         # they are "side by side" :)
@@ -57,23 +64,15 @@ class square:
         if bx < self.ax:
             return False
 
-
-        # # it is entirely inside:
-        # if ax > self.ax and self.bx > bx and ay > self.ay and self.by > by:
-        #     return False
-        # # it is entirely inside:
-        # if ax < self.ax and self.bx < bx and ay < self.ay and self.by < by:
-        #     return False
         if (ax == self.ax or ax == self.bx or bx == self.ax or bx == self.bx) and (self.ay <= ay <= self.by or self.ay <= by <= self.by or ay <= self.ay <= by or ay <= self.by <= by):
             return True
 
         return False
 
-    def intersects_horizontally(self, ax, ay, bx, by, sizex, sizey):  # dy should change
-
+    def intersects_horizontally(self, ax, ay, bx, by):  # dy should change
         if ay < 0:
             return True
-        if by >= sizey:
+        if by >= SIZE_Y:
             return True
 
         # they are "side by side" :)
@@ -82,28 +81,15 @@ class square:
         if by < self.ay:
             return False
 
-        # # it is entirely inside:
-        # if ax > self.ax and self.bx > bx and ay > self.ay and self.by > by:
-        #     return False
-        # # it is entirely inside:
-        # if ax < self.ax and self.bx < bx and ay < self.ay and self.by < by:
-        #     return False
         if (ay == self.ay or ay == self.by or by == self.ay or by == self.by ) and (self.ax <= ax <= self.bx or self.ax <= bx <= self.bx or ax <= self.ax <= bx or ax <= self.bx <= bx):
             return True
-
 
         return False
 
 
-
-
-
-
 class squares:
-    def __init__(self, sizex, sizey):
+    def __init__(self):
         self.squares = []
-        self.sizex = sizex
-        self.sizey = sizey
 
     def add_square(self, sq):
         self.squares += [sq]
@@ -113,7 +99,7 @@ class squares:
         for sq in self.squares:
             if sq == not_this_one:
                 continue
-            if sq.intersects(ax, ay, bx, by, self.sizex, self.sizey):
+            if sq.intersects(ax, ay, bx, by):
                return True
         return False
 
@@ -122,7 +108,7 @@ class squares:
         for sq in self.squares:
             if sq == not_this_one:
                 continue
-            if sq.intersects_vertically(ax, ay, bx, by, self.sizex, self.sizey):
+            if sq.intersects_vertically(ax, ay, bx, by):
                return True
         return False
 
@@ -131,21 +117,21 @@ class squares:
         for sq in self.squares:
             if sq == not_this_one:
                 continue
-            if sq.intersects_horizontally(ax, ay, bx, by, self.sizex, self.sizey):
+            if sq.intersects_horizontally(ax, ay, bx, by):
                return True
         return False
 
     def add_non_intersecting_square(self):
         ok = False
         for _ in range(1000):
-            ax = randrange(0, self.sizex)
-            ay = randrange(0, self.sizey)
+            ax = randrange(0, SIZE_X)
+            ay = randrange(0, SIZE_Y)
             size = randrange(8, 300)
             bx = ax + size
             by = ay + size
-            if bx >= self.sizex:
+            if bx >= SIZE_X:
                 continue
-            if by >= self.sizey:
+            if by >= SIZE_Y:
                 continue
             # verify existing squares conflicts
             if self.intersecting(ax, ay, bx, by):
@@ -191,31 +177,48 @@ class squares:
                 sq.by += sq.dy
 
     def generate_image(self):
-        size = self.sizex, self.sizey
-        im = Image.new('RGBA', size, (0, 0, 0, 255))
+        size = SIZE_X, SIZE_Y
+        im = Image.new('RGB', size, (0, 0, 0, 255))
         draw = ImageDraw.Draw(im)
         for sq in self.squares:
             draw.rectangle((sq.ax, sq.ay, sq.bx, sq.by), fill=None, width=1)
 
         return im
 
+    def generate_new_image(self):
+        print('.', end='', flush=True)
+        self.do_move()
+        return self.generate_image()
+
     def generate_video(self):
-        pass
+        im = self.generate_image()
+        # save as .gif
+        im.save(TEMP_FILE,
+                save_all=True,
+                append_images=[self.generate_new_image() for _ in range(FRAMES-1)],
+                duration=16.666,
+                loop=0,
+                minimize_size=True)
+
+        # Convert to video (.webm)
+        import moviepy.editor as mp
+        clip = mp.VideoFileClip(TEMP_FILE)
+        clip.write_videofile(OUTPUT_FILE, audio=False, threads=1, preset='fast',
+                             ffmpeg_params=['-vf', 'fps=60', '-ss', '5', '-crf', '30', '-b:v', '0'])
+        clip.close()
+
+        os.remove(TEMP_FILE)
 
 
 def main():
-    sqs = squares(1920, 1080)
-    for _ in range(100):
+    # generate squares
+    sqs = squares()
+    for _ in range(SQUARES):
         sqs.add_non_intersecting_square()
     print(sqs)
 
-    for i in range(500):
-        im = sqs.generate_image()
-        im.save(f'squares{i:04}.png', 'PNG')
-        sqs.do_move()
-        print(i)
+    sqs.generate_video()
 
 
 if __name__ == "__main__":
-    """ This is executed when run from the command line """
     main()
